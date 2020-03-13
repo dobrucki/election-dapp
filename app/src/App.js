33c021788273f2f69b022fbaf985a6fc100faee2
Jedrzej Dobrucki <dobrucki2@gmail.com>
2020-03-13 04:36:35 +0100

    Add sample navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Ballot from './Ballot';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Ballot></Ballot>
+      <Navbar />
     </div>
   );
 }
