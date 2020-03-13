dc91fb233458c0a2879785f695f2f8322e9d8af3
Jedrzej Dobrucki <dobrucki2@gmail.com>
2020-03-13 03:47:58 +0100

    Update react app Removed useless files.

---
+++ app/src/Ballot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Ballot = () => {
+    return(
+        <div className="Ballot">
+            <h1>Hello world</h1>
+        </div>
+    );
+}
+
+export default Ballot;
