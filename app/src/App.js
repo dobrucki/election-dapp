9d74a45d75da8b904fc7a455beacf299d1deb3aa
Jedrzej Dobrucki <dobrucki2@gmail.com>
2020-03-13 19:07:18 +0100

    Add navbar layout

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import Ballot from './Ballot';
-import Navbar from './Navbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
+import { makeStyles } from '@material-ui/core/styles';
+import Ballot from './components/Ballot';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+
+const useStyles = makeStyles(theme => ({
+  content: {
+    paddingRight: theme.spacing(20),
+    paddingLeft: theme.spacing(20)
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+      <BrowserRouter>
+        <Navbar />
+          <Switch className={classes.content}>
+            <Route path='/' exact component={Home} />
+            <Route path='/Ballot' component={Ballot} />
+            <Route path='/Results' component={Home} />
+            <Route component={Error} />
+          </Switch>
+      </BrowserRouter>
   );
 }
 
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import  { Link, NavLink } from 'react-router-dom';
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,19 +26,19 @@ const Navbar = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            election-dapp
           </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton edge='end' aria-label='account of current user' color='inherit'>
+            <AccountCircle />
+          </IconButton>
         </Toolbar>
       </AppBar>
-    </div>
   );
 }
 
